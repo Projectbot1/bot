672a8099c53d983f9373f673164de7203b26a74b
Projectbot1 <64796260+Projectbot1@users.noreply.github.com>
2020-05-04 21:34:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,53 +25,8 @@ async def on_member_remove(member):
   
 
  
-       @client.event
-async def on_member_join(member):
-    with open('money.json', 'r') as f:
-        users = json.load(f)
-
-
-    await update_data(users, member)
-
-    
-    with open('money.json', 'w') as f:
-        json.dump(users, f)
-
-
-
-@client.event
-async def on_message(message):
-    if message.author.bot == False:
-        with open('money.json', 'r') as f:
-            users = json.load(f)
-
-
-        await update_data(users, message.author)
-        await add_experience(users, message.author, 5)
-        await level_up(users, message.author, message)
     
     
-        with open('money.json', 'w') as f:
-            json.dump(users, f)
-
-
-async def update_data(users, user):
-    if not f'{user.id}' in users:
-        users[f'{user.id}'] = {}
-        users[f'{user.id}']['experience'] = 0
-        users[f'{user.id}']['level'] = 1
-
-
-async def add_experience(users, user, exp):
-    users[f'{user.id}']['experience'] += exp
-
-async def level_up(users, user, message):
-    experience = users[f'{user.id}']['experience']
-    lvl_start = users[f'{user.id}']['level']
-    lvl_end = int(experience ** (1/4))
-    if lvl_start < lvl_end:
-        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
-        users[f'{user.id}']['level'] = lvl_end
   
 
 @client.command(aliases=['8Ball','eightball','8ball'])
